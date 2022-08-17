0aee159df377f4aca4106c5d4a0cbc9ba44c7879
MohammedAlMuqbel <99381429+MohammedAlMuqbel@users.noreply.github.com>
2022-08-17 12:27:48 +0300

    Update My_face.py

--- My_face.py
+++ My_face.py
@@ -2,7 +2,7 @@ import cv2
 
 face_detect=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img=cv2.imread("Dania_2.jpg")
+img=cv2.imread("KoKy.jpg") ## example of image
 
 gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
